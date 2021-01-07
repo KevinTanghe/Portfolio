824cdb6baaa3a68f56818498b99fe5936fcee848
Kevin <tanghekevin1@gmail.com>
2021-01-07 17:10:51 +0100

    page portfolio terminé v1

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,37 @@ let aNav = document.getElementsByClassName("nav-link")
 let section = document.querySelectorAll("section")
 console.log(section);
 aNav[0].addEventListener("click", function(){
-    section[2].style.display = "block"
-    section[2].style.transition = "1s"
-    section[2].style.opacity = "1"
+    section[2].style.opacity = "0"
+    section[2].style.transition = "opacity 1s"
+    section[3].style.opacity = "0"
+    section[3].style.transition = "opacity 1s"
+    section[4].style.opacity = "0"
+    section[4].style.transition = "opacity 1s"
+    setTimeout(() => {
+        section[3].style.display = "none"
+        section[4].style.display = "none"
+        section[2].style.display = "block"
+    }, 1000);
+    setTimeout(() => {
+        section[2].style.opacity = "1"
+        section[2].style.transition = "opacity 1s"
+    }, 1500);
 })
 
 aNav[1].addEventListener("click", function(){
     section[2].style.opacity = "0"
     section[2].style.transition = "opacity 1s"
+    section[3].style.opacity = "0"
+    section[3].style.transition = "opacity 1s"
+    section[4].style.opacity = "0"
+    section[4].style.transition = "opacity 1s"
     setTimeout(() => {
         section[2].style.display = "none"
+        section[4].style.display = "none"
+        section[3].style.display = "block"
     }, 1000);
+    setTimeout(() => {
+        section[3].style.opacity = "1"
+        section[3].style.transition = "opacity 1s"
+    }, 1500);
 })
